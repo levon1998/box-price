068ec13f5bce834438c41a9054b339ead302dd09
levon1998 <Levon.babayan.a@gmail.com>
2018-12-23 21:39:04 +0400

    =add payment

--- resources/views/user/account/replenish-funds.blade.php
+++ resources/views/user/account/replenish-funds.blade.php
@@ -14,7 +14,6 @@
 
                         <div class="tab-content">
                             <div class="col-md-7">
-                                <a href="//www.free-kassa.ru/"><img src="//www.free-kassa.ru/img/fk_btn/4.png"></a>
                                 <iframe src="http://www.free-kassa.ru/merchant/forms.php?gen_form=1&m=113338&default-sum=200&button-text=Оплатить&encoding=CP1251&type=v3&id=271637" height="320" frameBorder="0" target="_parent" ></iframe>
                             </div>
                         </div>
