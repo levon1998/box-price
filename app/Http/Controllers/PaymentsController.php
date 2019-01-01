fd94efa72bdf02eb9db939ee0816397b90840979
levon1998 <Levon.babayan.a@gmail.com>
2019-01-02 02:52:10 +0400

    =pays

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -49,7 +49,7 @@ class PaymentsController extends Controller
         $data = ReplenishPays::select('user_id', 'pay')->where('order_id', $request->input('m_orderid'))->first();
         User::where('id', $data->user_id)->increment('balance', $data->pay);
         $user = User::select('balance')->where('id', $data->user_id)->first();
-        $request->session()->flash(['success' => true, 'balance' => $user->balance, 'sum' => $data->pay]);
+        $request->session()->flash('success', true);
         return redirect('/replenish-funds');
     }
 
@@ -60,7 +60,7 @@ class PaymentsController extends Controller
     public function replenishFundsFail(Request $request)
     {
         ReplenishPays::where('order_id', $request->input('m_orderid'))->delete();
-        $request->session()->flash(['success' => false]);
+        $request->session()->flash('success', false);
         return redirect('/replenish-funds');
     }
 }
