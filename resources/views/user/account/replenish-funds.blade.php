d83b7e3c45dbaf4312097d0e0d2d13783a9f3625
levon1998 <Levon.babayan.a@gmail.com>
2019-01-02 02:55:38 +0400

    =pays

--- resources/views/user/account/replenish-funds.blade.php
+++ resources/views/user/account/replenish-funds.blade.php
@@ -11,7 +11,7 @@
 
                 @if (!empty(Session::get('success')) && Session::get('success'))
                     <div class="alert alert-success" role="alert">
-                        Поздравляем. ваш счет успешно пополнен на {{ session('sum') }} сумму. Текущее баланс составляет {{ session('balance') }}.
+                        Поздравляем. ваш счет успешно пополнен. Текущее баланс составляет {{ Auth::user()->balance }}.
                     </div>
                 @elseif (!empty(Session::get('success')) && !Session::get('success'))
                     <div class="alert alert-danger" role="alert">
