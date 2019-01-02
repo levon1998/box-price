5c15a798ef138e7b655a0d0c25ab5841139c66f6
levon1998 <Levon.babayan.a@gmail.com>
2019-01-03 01:42:58 +0400

    =add 20%

--- database/migrations/2018_12_31_145346_create_replenish_pays_table.php
+++ database/migrations/2018_12_31_145346_create_replenish_pays_table.php
@@ -18,7 +18,7 @@ class CreateReplenishPaysTable extends Migration
             $table->integer('user_id');
             $table->string('order_id');
             $table->float('pay', 8, 2);
-            $table->float('total', 8, 2);
+            $table->float('total', 8, 2)->nullable();
             $table->tinyInteger('status');
             $table->timestamps();
         });
