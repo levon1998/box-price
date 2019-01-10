3470dbfc072b018ea041f5a5ebaf33fb65bc0a3e
levon1998 <Levon.babayan.a@gmail.com>
2019-01-11 00:13:25 +0400

    =small changes

---
+++ database/migrations/2019_01_08_194615_create_statistics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatisticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statistics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('visits_count');
+            $table->integer('users_count');
+            $table->integer('pay_in');
+            $table->integer('pay_out');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistics');
+    }
+}
